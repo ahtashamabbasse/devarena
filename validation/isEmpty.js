7892b594136a4909154681ba94db50c4884c6cc1
ahtashamabbasse <ch.ahtasham123@gmail.com>
2019-07-13 20:01:04 +0500

    registration validation complete

--- validation/isEmpty.js
+++ validation/isEmpty.js
@@ -1,7 +1,7 @@
 const isEmpty = value =>
-    typeof value === undefined ||
-    typeof value === null ||
-    (typeof value === "object" && Object.keys(value).length === 0)
+    value === undefined ||
+    value === null ||
+    (typeof value === "object" && Object.keys(value).length === 0) ||
     (typeof value === "string" && value.trim().length === 0);
 
 
