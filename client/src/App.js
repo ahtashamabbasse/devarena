e6e926120f5f89af53ec4b136a7555a649d38b66
ahtashamabbasse <ch.ahtasham123@gmail.com>
2019-07-15 23:33:50 +0500

    register and login router added

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,26 @@ import Navbar from "./components/layouts/Navbar";
 import Footer from "./components/layouts/Footer";
 import Landing from "./components/layouts/Landing";
 import {BrowserRouter as Router, Route} from "react-router-dom";
+import Register from "./components/auth/register";
+import Login from "./components/auth/login";
 
 function App() {
     return (
         <div>
             <Router>
-                <Navbar/>
-                <Route exact path={'/'} component={Landing}/>
-                <Footer/>
+                <div className={'app'}>
+                    <Navbar/>
+                    <Route exact path={'/'} component={Landing}/>
+                    <div className="container">
+                        <Route exact path={'/register'} component={Register}/>
+                        <Route exact path={'/login'} component={Login}/>
+
+                    </div>
+                    <Footer/>
+                </div>
             </Router>
         </div>
-    );
+);
 }
 
 export default App;
