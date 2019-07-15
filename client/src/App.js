3e2d153e4d7fbba516a2c152e1d16a205bc08f1e
ahtashamabbasse <ch.ahtasham123@gmail.com>
2019-07-15 23:23:35 +0500

    added router

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,18 @@ import './App.css';
 import Navbar from "./components/layouts/Navbar";
 import Footer from "./components/layouts/Footer";
 import Landing from "./components/layouts/Landing";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 
 function App() {
-  return (
-    <div>
-        <Navbar/>
-        <Landing/>
-        <Footer/>
-    </div>
-  );
+    return (
+        <div>
+            <Router>
+                <Navbar/>
+                <Route exact path={'/'} component={Landing}/>
+                <Footer/>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
