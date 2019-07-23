18143ab5d6c60b3c151d18fb24e07fa00297fd32
ahtashamabbasse <ch.ahtasham123@gmail.com>
2019-07-23 23:53:28 +0500

    created CreateProfile

---
+++ client/src/components/create-profile/CreateProfile.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class CreateProfile extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default CreateProfile;
