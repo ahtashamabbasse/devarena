d586e7211d256a1ac03c7859ded51d975a93d897
ahtashamabbasse <ch.ahtasham123@gmail.com>
2019-07-23 23:59:19 +0500

    add states

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -1,6 +1,31 @@
 import React, {Component} from 'react';
+import {connect} from "react-redux";
 
 class CreateProfile extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state={
+            displaySocialInput:false,
+            handle:"",
+            company:"",
+            website:"",
+            location:"",
+            status:"",
+            skills:"",
+            githubUsername:"",
+            bio:"",
+            twitter:"",
+            facebook:"",
+            instagram:"",
+            linkedin:"",
+            youtube:"",
+            errors:{},
+
+        }
+    }
+
+
     render() {
         return (
             <div>
@@ -10,4 +35,9 @@ class CreateProfile extends Component {
     }
 }
 
-export default CreateProfile;
+const mapStateToProps=(state)=>({
+   profile:state.profile,
+   errors:state.errors,
+
+});
+export default connect()(CreateProfile);
