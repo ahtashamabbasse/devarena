4af8bbdd0d16f2c83bbee9f076a4fd520321def2
ahtashamabbasse <ch.ahtasham123@gmail.com>
2019-08-09 23:52:44 +0500

    update

--- client/src/components/not-found.js
+++ client/src/components/not-found.js
@@ -5,6 +5,7 @@ function NotFound(props) {
         <div>
             <h1 className={'d-4 text-center'}>Page Not Found</h1>
             <p className={'text-center'}>Sorry, This page isn't found</p>
+
         </div>
     );
 }
