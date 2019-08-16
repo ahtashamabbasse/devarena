eb1abaaa1224c8d58e4f5e2b4b8e178cb3e4bcca
ahtashamabbasse <ch.ahtasham123@gmail.com>
2019-08-16 15:45:59 +0500

    deployment issue fix

--- server/app.js
+++ server/app.js
@@ -44,13 +44,13 @@ app.use('/api/profile', profileRouter);
 app.use('/api/posts', postRouter);
 
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
-
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-    })
-}
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('client/build'))
+//
+//     app.get('*',(req,res)=>{
+//         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+//     })
+// }
 
 
 // catch 404 and forward to error handler
