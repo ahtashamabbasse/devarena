d18af76ef56fb18afabb6f71c8e39067bcec85e1
ahtashamabbasse <ch.ahtasham123@gmail.com>
2020-02-25 23:53:59 +0500

    User registration test

---
+++ server/__tests__/profile.test.js
@@ -0,0 +1,19 @@
+const request = require('supertest');
+const app = require('../app');
+
+
+describe('User Authentication', () => {
+
+    it('Register the user', async () => {
+        const res = await request(app)
+            .post('/api/users/register')
+            .send({
+                name: 'David',
+                email: 'abc@gmail.com',
+                password:'123456'
+            });
+        expect(res.statusCode).toEqual(201)
+        expect(res.body).toHaveProperty('post')
+    })
+
+})
