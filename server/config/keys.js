679a665ad06d49afa6950fc28404801d3f0fddd2
ahtashamabbasse <ch.ahtasham123@gmail.com>
2020-02-25 23:37:32 +0500

    staging env configure

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,6 +1,8 @@
-require('dotenv').config()
+require('dotenv').config();
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'production')
     module.exports = require('./keys_prod');
 else if (process.env.NODE_ENV === 'development')
     module.exports = require('./keys_dev');
+else if (process.env.NODE_ENV === 'staging')
+    module.exports = require('./keys_staging');
